08aa64d6690d1d396256957cababb632d0a9d31d
Jose Miguel Venegas Mendoza <jvenegasmendoza@gmail.com>
2016-10-14 16:00:55 -0500

    Adding background image.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import sys, pygame
+import os, sys, pygame
 
 
 def main():
@@ -8,9 +8,24 @@ def main():
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption('Pong Pygame')
 
+    try:
+        filename = os.path.join(
+            os.path.dirname(__file__),
+            'assets',
+            'graphics',
+            'background.png')
+        background = pygame.image.load(filename)
+        background = background.convert()
+    except pygame.error as e:
+        print ('Cannot load image: ', filename)
+        raise SystemExit(str(e))
+
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
+
+        screen.blit(background, (0, 0))
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
