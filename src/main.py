285bfec1c6b3ed1f7eccee6a6fc9953f0dca0259
Jose Miguel Venegas Mendoza <jvenegasmendoza@gmail.com>
2016-10-20 18:14:32 -0500

    Implementing scores.

--- src/main.py
+++ src/main.py
@@ -27,9 +27,10 @@ class Pad(pygame.sprite.Sprite):
 class Ball(pygame.sprite.Sprite):
     def __init__(self, pos=(0, 0)):
         pygame.sprite.Sprite.__init__(self)
+        self.pos = pos
         self.image = pygame.Surface((10, 10)).convert()
         self.image.fill((255, 255, 255))
-        self.rect = self.image.get_rect(center=pos)
+        self.rect = self.image.get_rect(center=self.pos)
         self.speed_x = 0
         self.speed_y = 0
 
@@ -47,8 +48,28 @@ class Ball(pygame.sprite.Sprite):
         self.speed_x = 0
         self.speed_y = 0
 
+    def reset(self):
+        self.rect = self.image.get_rect(center=self.pos)
+
     def update(self):
         self.rect.move_ip(self.speed_x, self.speed_y)
+
+
+class Score(pygame.sprite.Sprite):
+    def __init__(self, font, pos=(0, 0)):
+        pygame.sprite.Sprite.__init__(self)
+        self.font = font
+        self.pos = pos
+        self.score = 0
+        self.image = self.font.render(str(self.score), 0, (255, 255, 255))
+        self.rect = self.image.get_rect(center=self.pos)
+
+    def score_up(self):
+        self.score += 1
+
+    def update(self):
+        self.image = self.font.render(str(self.score), 0, (255, 255, 255))
+        self.rect = self.image.get_rect(center=self.pos)
 
 
 def main():
@@ -74,7 +95,25 @@ def main():
     pad_right = Pad((5*width/6, 3*height/4))
     ball = Ball((width/2, height/2))
 
-    sprites = pygame.sprite.Group(pad_left, pad_right, ball)
+    if not pygame.font:
+        raise SystemExit('Pygame does not support fonts')
+
+    try:
+        filename = os.path.join(
+            os.path.dirname(__file__),
+            'assets',
+            'fonts',
+            'wendy.ttf')
+        font = pygame.font.Font(filename, 90)
+    except pygame.error as e:
+        print ('Cannot load font: ', filename)
+        raise SystemExit(str(e))
+
+    left_score = Score(font, (width/3, height/8))
+    right_score = Score(font, (2*width/3, height/8))
+
+    sprites = pygame.sprite.Group(
+        pad_left, pad_right, ball, left_score, right_score)
 
     clock = pygame.time.Clock()
     fps = 60
@@ -83,6 +122,8 @@ def main():
 
     top = pygame.Rect(0, 0, width, 5)
     bottom = pygame.Rect(0, height-5, width, 5)
+    left = pygame.Rect(0, 0, 5, height)
+    right = pygame.Rect(width-5, 0, 5, height)
 
     while 1:
         clock.tick(fps)
@@ -114,6 +155,15 @@ def main():
         screen_rect = screen.get_rect().inflate(0, -10)
         pad_left.rect.clamp_ip(screen_rect)
         pad_right.rect.clamp_ip(screen_rect)
+
+        if ball.rect.colliderect(left):
+            right_score.score_up()
+            ball.reset()
+            ball.stop()
+        elif ball.rect.colliderect(right):
+            left_score.score_up()
+            ball.reset()
+            ball.stop()
 
         sprites.update()
 
